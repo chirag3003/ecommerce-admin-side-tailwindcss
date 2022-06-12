50cc2bc91d72e6a38e8ce10a1ae5dcc3a2b4ab7b
Chirag Bhalotia <me@chirag.codes>
2022-06-12 12:55:04 +0530

    .

---
+++ src/components/ErrorRequesAlert/index.jsx
@@ -0,0 +1,35 @@
+import { ExclamationCircleIcon } from "@heroicons/react/solid";
+import React from "react";
+import { useEffect } from "react/cjs/react.production.min";
+
+function ErrorRequestAlert({ message, open, setOpen, time = 2000 }) {
+    async function close() {
+        setTimeout(() => {
+            setOpen(false);
+        }, time);
+    }
+    useEffect(() => {
+        if (open) close();
+    }, [open]);
+    return (
+        <div
+            className={`rounded-md bg-red-50 p-4 fixed bottom-2 right-2 ml-2 ${
+                !open ? "hidden" : ""
+            }`}
+        >
+            <div className="flex">
+                <div className="flex-shrink-0">
+                    <ExclamationCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+                </div>
+                <div className="ml-3">
+                    <h3 className="text-sm font-medium text-red-800">Request completed</h3>
+                    <div className="mt-2 text-sm text-red-700">
+                        <p>{message}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ErrorRequestAlert;
--- src/components/Gallery/AddModal.jsx
+++ src/components/Gallery/AddModal.jsx
@@ -1,36 +1,42 @@
 /* This example requires Tailwind CSS v2.0+ */
-import {Fragment, useContext, useRef, useState} from "react";
+import { Fragment, useContext, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { ExclamationIcon } from "@heroicons/react/outline";
-import {PhotographIcon} from "@heroicons/react/solid";
+import { PhotographIcon } from "@heroicons/react/solid";
 import Auth from "@helpers/Auth";
+import SuccessfulRequestAlert from "@/SuccesfulRequestAlert";
+import ErrorRequestAlert from "@/ErrorRequesAlert";
 
 export default function AddModal({ open, setOpen }) {
-    const auth = useContext(Auth)
-    const [imageURL,setImageURL] = useState("")
-    const [image,setImage] = useState("")
+    const auth = useContext(Auth);
+    const [imageURL, setImageURL] = useState("");
+    const [image, setImage] = useState("");
+    const [successOpen, setSuccessOpen] = useState(false);
+    const [errorOpen, setErrorOpen] = useState(false);
 
-    function changeImage(evt){
-        setImage(evt.target.files[0])
-        let url = URL.createObjectURL(evt.target.files[0])
-        setImageURL(url)
+    function changeImage(evt) {
+        setImage(evt.target.files[0]);
+        let url = URL.createObjectURL(evt.target.files[0]);
+        setImageURL(url);
     }
 
-    function addImage(evt){
-        evt.preventDefault()
-        let data = new FormData()
-        data.append("name",evt.target[0].value)
-        data.append("image",image)
-        auth.Axios.post("/images/gallery",data).then(res => {console.log(res)}).catch(err => console.error(err))
+    function addImage(evt) {
+        evt.preventDefault();
+        let data = new FormData();
+        data.append("name", evt.target[0].value);
+        data.append("image", image);
+        auth.Axios.post("/images/gallery", data)
+            .then((res) => {
+                setSuccessOpen(true);
+                setOpen(false);
+            })
+            .catch((err) => {
+                setErrorOpen(true);
+            });
     }
 
     return (
         <Transition.Root show={open} as={Fragment}>
-            <Dialog
-                as="div"
-                className="fixed z-10 inset-0 overflow-y-auto"
-                onClose={setOpen}
-            >
+            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setOpen}>
                 <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <Transition.Child
                         as={Fragment}
@@ -60,7 +66,10 @@ export default function AddModal({ open, setOpen }) {
                         leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     >
-                        <form onSubmit={addImage}    className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                        <form
+                            onSubmit={addImage}
+                            className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+                        >
                             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                 <div className="sm:flex sm:items-start">
                                     <div className="mx-auto flex-shrink-0 flex items-center justify-center bg-blue-200 h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10">
@@ -77,7 +86,10 @@ export default function AddModal({ open, setOpen }) {
                                             Add an Image to the gallery
                                         </Dialog.Title>
                                         <div className="mt-2 w-full">
-                                            <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                                            <label
+                                                htmlFor="name"
+                                                className="block text-sm font-medium text-gray-700"
+                                            >
                                                 Name
                                             </label>
                                             <div className="mt-1">
@@ -92,29 +104,41 @@ export default function AddModal({ open, setOpen }) {
                                             </div>
                                         </div>
                                         <div className="mt-2 w-full">
-                                        <label  htmlFor="file-upload" className="mt-2 w-full">
-                                            <div style={{
-                                                backgroundImage:(imageURL)?`url("${imageURL}")`:"",
-                                                backgroundPosition:"center center",
-                                                backgroundSize:"contain",
-                                                backgroundRepeat:"no-repeat"
-                                                }} className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md"
-                                            >
-                                                <div className="space-y-1 text-center">
-                                                    <PhotographIcon
-                                                        className="mx-auto h-12 w-12 text-gray-400"
-                                                    />
-                                                    <div className="flex text-sm text-gray-600">
-                                                        <span>Upload a file</span>
+                                            <label htmlFor="file-upload" className="mt-2 w-full">
+                                                <div
+                                                    style={{
+                                                        backgroundImage: imageURL
+                                                            ? `url("${imageURL}")`
+                                                            : "",
+                                                        backgroundPosition: "center center",
+                                                        backgroundSize: "contain",
+                                                        backgroundRepeat: "no-repeat",
+                                                    }}
+                                                    className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md"
+                                                >
+                                                    <div className="space-y-1 text-center">
+                                                        <PhotographIcon className="mx-auto h-12 w-12 text-gray-400" />
+                                                        <div className="flex text-sm text-gray-600">
+                                                            <span>Upload a file</span>
 
-                                                            <input onChange={changeImage} required={true} id="file-upload" accept="image/png, image/gif, image/jpeg, image/webp" name="images" type="file" className="sr-only" />
+                                                            <input
+                                                                onChange={changeImage}
+                                                                required={true}
+                                                                id="file-upload"
+                                                                accept="image/png, image/gif, image/jpeg, image/webp"
+                                                                name="images"
+                                                                type="file"
+                                                                className="sr-only"
+                                                            />
 
-                                                        <p className="pl-1">or drag and drop</p>
+                                                            <p className="pl-1">or drag and drop</p>
+                                                        </div>
+                                                        <p className="text-xs text-gray-500">
+                                                            PNG, JPG, GIF up to 10MB
+                                                        </p>
                                                     </div>
-                                                    <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
                                                 </div>
-                                            </div>
-                                        </label>
+                                            </label>
                                         </div>
                                     </div>
                                 </div>
@@ -123,7 +147,6 @@ export default function AddModal({ open, setOpen }) {
                                 <button
                                     type="submit"
                                     className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 mb-2 lg:mb-0 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-
                                 >
                                     Add Image
                                 </button>
@@ -134,11 +157,20 @@ export default function AddModal({ open, setOpen }) {
                                 >
                                     Cancel
                                 </button>
-
                             </div>
                         </form>
                     </Transition.Child>
                 </div>
+                {/* <ErrorRequestAlert
+                    open={errorOpen}
+                    setOpen={setErrorOpen}
+                    message={"Name already in use"}
+                /> */}
+                <SuccessfulRequestAlert
+                    open={successOpen}
+                    setOpen={setSuccessOpen}
+                    message={"Image added  succesfully "}
+                />
             </Dialog>
         </Transition.Root>
     );
